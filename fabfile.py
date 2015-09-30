3116fd82404b31a1ac02f41fdeb0dbfa9e088998
Michel Albert <michel@albert.lu>
2015-09-30 18:18:56 +0200

    Added a "publish" task for fabric.

--- fabfile.py
+++ fabfile.py
@@ -6,3 +6,10 @@ def doc():
     fab.local('./env/bin/sphinx-build '
               'docs '
               'docs/_build/html')
+
+
+@fab.task
+def publish():
+    fab.local('./env/bin/python setup.py sdist bdist_wheel')
+    fab.local('./env/bin/twine upload dist/*')
+    fab.local('rm -rf dist')
