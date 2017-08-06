12f0a6c92597da51b74bff9a796b193ededca2b9
Michel Albert <michel@albert.lu>
2017-08-06 10:33:47 +0200

    Add a filter to shift logging levels

---
+++ gouge/filters.py
@@ -0,0 +1,45 @@
+"""
+This module contains generally useful filters.
+"""
+import logging
+from warnings import warn
+
+
+class ShiftingFilter:
+    """
+    This filter will shift the logging level of log records a certain number of
+    log levels.
+
+    :param shift_by: The number of levels to shift. Positive integers will
+        increase the level, negative integers will decrease it.
+    :param logger: This and all child loggers will be shifted.
+    :param min: Don't shift below this level.
+    :param max: Don't shift above this level.
+    :param offset: An explicit, fine-grained offset value. This overrides
+        *shift_by*!
+    """
+    def __init__(self, shift_by=0, logger='',
+                 min=logging.NOTSET, max=logging.CRITICAL, offset=0):
+        if offset and shift_by:
+            warn('You specified both "offset" and "shift_by"! "shift_by" '
+                 'will be ignored!', SyntaxWarning)
+
+        if offset:
+            self.offset = offset
+        else:
+            self.offset = 10 * shift_by
+        self.logger = logger
+        self.max = max
+        self.min = min
+
+    def filter(self, record):
+        """
+        Always returns *True* but will modify the logging level of *record* by
+        the rules defined in this filter.
+
+        See :py:meth:`logging.filters.Filter.filter`
+        """
+        if record.name.startswith(self.logger):
+            new_level = record.levelno + self.offset
+            record.levelno = min(self.max, max(self.min, new_level))
+        return True
