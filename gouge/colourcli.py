aee2580ad382215a9b6ada60aecfeab6348fa083
Michel Albert <michel@albert.lu>
2018-12-29 10:28:50 +0100

    Sphinx/rst markup fix

--- gouge/colourcli.py
+++ gouge/colourcli.py
@@ -13,7 +13,7 @@ class Simple(logging.Formatter):
     .. note:: This formatter *suppresses* tracebacks by default! Remember that
         is is meant to give a concise, readable output. If you need to see
         tracebacks on the console, you can override this setting useing
-        \*show_exc\*.
+        \\*show_exc\\*.
     """
 
     @staticmethod
