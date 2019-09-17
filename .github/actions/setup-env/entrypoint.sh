1ef3aa147165c8b392251076c286bc9aaab483fd
Michel Albert <michel@albert.lu>
2019-09-17 18:38:57 +0200

    Add missing test dependency

--- .github/actions/setup-env/entrypoint.sh
+++ .github/actions/setup-env/entrypoint.sh
@@ -2,6 +2,6 @@
 
 sh -c "python3 -m venv env"
 sh -c "./env/bin/pip install -e ."
-sh -c "./env/bin/pip install pytest"
+sh -c "./env/bin/pip install pytest blessings"
 sh -c "./env/bin/pytest test"
 
