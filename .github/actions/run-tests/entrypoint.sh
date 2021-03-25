96728e7a912247631ed4a64bb203e076723ee566
Michel Albert <michel.albert@post.lu>
2021-03-25 08:02:29 +0100

    Update github actions

--- .github/actions/run-tests/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -l
-
-sh -c "./env/bin/pytest test"
--- .github/actions/setup-env/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-sh -c "python3 -m venv env"
-sh -c "./env/bin/pip install -e ."
-sh -c "./env/bin/pip install pytest blessings"
-sh -c "./env/bin/pytest test"
-
