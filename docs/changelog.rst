1bfe0e14d52e5941e678b4110bd59dfc86b94171
Michel Albert <michel@albert.lu>
2023-07-09 13:12:53 +0200

    Add deprecation warning

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+Version 1.5.0.post2
+-------------
+
+* Mark 1.x as deprecated
+
 Version 1.5.0
 -------------
 
--- gouge/__init__.py
+++ gouge/__init__.py
@@ -1,3 +1,9 @@
 from pkg_resources import resource_string
+from warnings import warn
 
 __version__ = resource_string("gouge", "version.txt").decode("ascii").strip()
+warn(
+    "gouge 1.x is deprecated and no longer maintained. Please upgrade to 2.x",
+     DeprecationWarning,
+     stacklevel=2
+)
