0bb48b88e12adea83c19b0d758918586a88530a4
Michel Albert <michel@albert.lu>
2023-07-09 12:01:56 +0200

    Remove unused imports

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
-import shlex
-import sys
 from importlib import metadata
 
 # If extensions (or modules to document with autodoc) are in another directory,
